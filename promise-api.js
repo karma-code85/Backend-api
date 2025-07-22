2539099e298f14f87da263a3e57a0601ff487e54
Marwan Mohamed <ranmoh85@gmail>
2025-07-22 10:27:53 +0200

    parall promise

--- promise-api.js
+++ promise-api.js
@@ -1,4 +1,14 @@
-const p= Promise.resolve({id: 1})
-p.then(result=>console.log(result))
-const pro=Promise.reject(new Error('resons for rejaction'))
-pro.catch(err=>console.log(err))
+const p1= new Promise((resolve)=>{
+setTimeout(()=>{
+    console.log('Async opration1...')
+    resolve(1);
+},2000)
+})
+const p2= new Promise((resolve)=>{
+setTimeout(()=>{
+    console.log('Async opration2...')
+    resolve(2);
+},2000)
+})
+Promise.race([p1,p2])
+.then(result=>console.log(result))
